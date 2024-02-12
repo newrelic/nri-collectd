d7d2842abd1fd00521bfe7f165200497ebafdd91
Mike Summers <msummers@newrelic.com>
2024-02-12 12:39:53 -0600

    Add ability to send to Metric API endpoint

--- collectd-plugin.go
+++ collectd-plugin.go
@@ -19,10 +19,11 @@ import (
 
 type argumentList struct {
    sdkArgs.DefaultArgumentList
-   Port     int    `default:"25826" help:"Port to listen on"`
-   Key      string `default:"" help:"Insights Insert Key required for sending dimensional metrics"`
-   Interval string `default:"15s" help:"Interval to harvest dimensional metrics"`
-   Dim      bool   `default:"false" help:"Report output as dimensional metrics (true|false)"`
+   Port         int    `default:"25826" help:"Port to listen on"`
+   Key          string `default:"" help:"Insights Insert Key required for sending dimensional metrics"`
+   Interval     string `default:"15s" help:"Interval to harvest dimensional metrics"`
+   Dim          bool   `default:"false" help:"Report output as dimensional metrics (true|false)"`
+   MetricApiUrl string `default:"https://metric-api.newrelic.com/metric/v1" help:"Metric API endpoint to use"`
 }
 
 const (
@@ -37,12 +38,29 @@ type CollectDReceiver struct {
    metricChannel chan *api.ValueList
 }
 
+// Write captures the valueLists from collectd and places them on the channel
+func (receiver *CollectDReceiver) Write(_ context.Context, valueLists *api.ValueList) error {
+   receiver.metricChannel <- valueLists
+   return nil
+}
+
+// Stateful nri- this nri never exits
+func main() {
+   collectdIntegration, err := integration.New(integrationName, integrationVersion, integration.Args(&args))
+   fatalIfErr(err)
+
+   if args.All() || args.Metrics {
+      fatalIfErr(populateMetrics(collectdIntegration))
+   }
+   fatalIfErr(collectdIntegration.Publish())
+}
+
 func populateMetrics(integration *integration.Integration) error {
    ListenAndWrite(integration)
    return nil
 }
 
-// ListenAndWrite starts the collectd receiver
+// ListenAndWrite starts the collectd receiver, it never exits but listens constantly
 func ListenAndWrite(integration *integration.Integration) {
    var metricChannel = make(chan *api.ValueList)
    writer := CollectDReceiver{metricChannel: metricChannel}
@@ -63,11 +81,6 @@ func ListenAndWrite(integration *integration.Integration) {
    log.Fatal(srv.ListenAndWrite(context.Background()))
 }
 
-func (receiver *CollectDReceiver) Write(_ context.Context, valueLists *api.ValueList) error {
-   receiver.metricChannel <- valueLists
-   return nil
-}
-
 func startDimMetricsProcessor(metricChannel chan *api.ValueList) {
    t, _ := time.ParseDuration(args.Interval)
 
@@ -75,6 +88,7 @@ func startDimMetricsProcessor(metricChannel chan *api.ValueList) {
       telemetry.ConfigAPIKey(args.Key),
       telemetry.ConfigHarvestPeriod(t),
       telemetry.ConfigBasicErrorLogger(os.Stdout),
+      telemetry.ConfigMetricsURLOverride(args.MetricApiUrl),
    )
    if err != nil {
       log.Fatal(err)
@@ -86,12 +100,12 @@ func startDimMetricsProcessor(metricChannel chan *api.ValueList) {
       // for _, vl := range valueLists {
       // println(vl.Identifier.Plugin + " " + vl.Identifier.PluginInstance + "  " + vl.Identifier.Type + "  " + vl.Identifier.TypeInstance)
       metricName := fmt.Sprintf("%s.%s", vl.Identifier.Type, vl.Identifier.TypeInstance)
-      recordMetric(vl, h, metricName)
+      recordDimensionalMetric(vl, h, metricName)
       // }
    }
 }
 
-func recordMetric(vl *api.ValueList, h *telemetry.Harvester, metricName string) {
+func recordDimensionalMetric(vl *api.ValueList, h *telemetry.Harvester, metricName string) {
    for _, val := range vl.Values {
       switch val.(type) {
       case api.Counter:
@@ -167,16 +181,6 @@ func startInfraMetricProcessor(metricChannel chan *api.ValueList, integration *i
       // }
       fatalIfErr(integration.Publish())
    }
-}
-
-func main() {
-   collectdIntegration, err := integration.New(integrationName, integrationVersion, integration.Args(&args))
-   fatalIfErr(err)
-
-   if args.All() || args.Metrics {
-      fatalIfErr(populateMetrics(collectdIntegration))
-   }
-   fatalIfErr(collectdIntegration.Publish())
 }
 
 func fatalIfErr(err error) {
